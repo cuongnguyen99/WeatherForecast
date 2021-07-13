27b9bb343828355d37f9759aaa155ffb1a9ba87b
ninhhaihoang <60962831+ninhhaihoang@users.noreply.github.com>
2021-07-13 11:10:42 +0700

    Update Wind.java

--- app/src/main/java/com/midterm/weatherforecast/model/Wind.java
+++ app/src/main/java/com/midterm/weatherforecast/model/Wind.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+// G
+
 public class Wind implements Serializable{
     @SerializedName("speed")
     private Double speed;
